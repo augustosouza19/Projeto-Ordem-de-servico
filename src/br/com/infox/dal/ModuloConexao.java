51efd6b70a5652bd4dac8035f2b22c118a8166f2
Augusto <Augusto@192.168.1.10>
2019-05-02 19:24:12 -0300

    Alteração

---
+++ src/br/com/infox/dal/ModuloConexao.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.infox.dal;
+
+import java.sql.*;
+
+/**
+ *
+ * @author Aluno
+ */
+public class ModuloConexao {
+
+    public static Connection conector() {
+        java.sql.Connection conexao = null;
+        String driver = "com.mysql.jdbc.Driver";
+        String url = "jdbc:mysql://localhost:3306/dbinfox";
+        String user = "root";
+        String password = "";
+        try {
+            Class.forName(driver);
+            conexao = DriverManager.getConnection(url, user, password);
+            return conexao;
+        } catch (Exception e) {
+            return null;
+        }
+    }
+}
